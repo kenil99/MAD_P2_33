fce613d9dccef057c8070c6d967f9cf25332bc2d
student <student@gnu.ac.in>
2023-07-31 13:12:18 +0530

    Initial commit

---
+++ app/src/main/java/com/example/p2_mad_33/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.p2_mad_33
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+
+
+class MainActivity : AppCompatActivity() {
+    val TAG = "MainActivity"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        showMessage("onCreate() method is called")
+    }
+    override fun onStart() {
+        super.onStart()
+        showMessage("onStart() method is called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        showMessage("OnResume() method is called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        showMessage("OnPause() method is called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        showMessage("OnStop() method is called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        showMessage("OnRestart() method is called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        showMessage("OnDestroy() method is called")
+    }
+
+    fun showMessage(msg:String){
+        Log.i(TAG, "$msg")
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+        //Snackbar.make(findViewById(androidx.constraintlayout.widget.R.id.constraint), "$msg", Snackbar.LENGTH_SHORT).show()
+    }
+}
